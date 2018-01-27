9568585967230d23e42c4a14502da720c9ca755f
Dallas Crenshaw <dallas.crenshaw@students.thalesacademy.org>
2018-01-27 14:39:16 -0800

    Fixed Up Naming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCornerAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCornerAutonomous.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="BlueDEVAutonomous", group="Linear Opmode")
+@Autonomous(name="BlueCornerAutonomous", group="Linear Opmode")
 //@Disabled
 
 public class BlueCornerAutonomous extends LinearOpMode {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLaneAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLaneAutonomous.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="BlueDEVAutonomous", group="Linear Opmode")
+@Autonomous(name="BlueLaneAutonomous", group="Linear Opmode")
 //@Disabled
 
 public class BlueLaneAutonomous extends LinearOpMode {
